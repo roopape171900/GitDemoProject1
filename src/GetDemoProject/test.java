dc2be0320c6f396dee9c2a3a83a49fda44c3ff65
roopa <roopape171900@gmail.com>
2024-02-14 22:10:41 +0530

    First commit...

---
+++ src/GetDemoProject/test.java
@@ -0,0 +1,18 @@
+package GetDemoProject;
+
+public class test {
+	
+
+		public static void main(String[] args) {
+		
+			String hungry = "yes";
+			if(hungry=="yes")
+				System.out.println("eat something.,,");
+			else {
+				System.out.println("do work.......");
+			}
+		}
+
+	}
+
+
